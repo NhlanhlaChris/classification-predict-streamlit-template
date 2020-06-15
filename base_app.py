0ea961fce1a9b7409a022234a888bcd2732e1d5c
Sibonelo.Malakiya <sbonelojunior@gmail.com>
2020-06-15 12:51:20 +0200

    added pipeline model

--- base_app.py
+++ base_app.py
@@ -1,7 +1,7 @@
 """
 
     Simple Streamlit webserver application for serving developed classification
-	models.
+    models.
 
     Author: Explore Data Science Academy.
 
@@ -13,71 +13,105 @@
     ---------------------------------------------------------------------
 
     Description: This file is used to launch a minimal streamlit web
-	application. You are expected to extend the functionality of this script
-	as part of your predict project.
+    application. You are expected to extend the functionality of this script
+    as part of your predict project.
 
-	For further help with the Streamlit framework, see:
+    For further help with the Streamlit framework, see:
 
-	https://docs.streamlit.io/en/latest/
+    https://docs.streamlit.io/en/latest/
 
 """
 # Streamlit dependencies
 import streamlit as st
 import joblib,os
+from PIL import Image
 
 # Data dependencies
 import pandas as pd
 
-# Vectorizer
-news_vectorizer = open("resources/tfidfvect.pkl","rb")
-tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl file
+# Load pipeline model
+
+model = joblib.load('resources/lscv_model.pkl')
 
 # Load your raw data
 raw = pd.read_csv("resources/train.csv")
 
+# Function to load images
+# Images
+def load_images(file_name):
+	img = Image.open(file_name)
+	return st.image(img,width=100)
+
 # The main function where we will build the actual app
 def main():
-	"""Tweet Classifier App with Streamlit """
+    """Tweet Classifier App with Streamlit """
 
-	# Creates a main title and subheader on your page -
-	# these are static across all pages
-	st.title("Tweet Classifer")
-	st.subheader("Climate change tweet classification")
+    # Creates a main title and subheader on your page -
+    # these are static across all pages
+    # st.title("Climate Change Belief Predictor")
+    # st.subheader("Predict if a person believes in climate change based on thier tweet")
+    
+    html_temp = """
+	<div style="background-color:lightblue;padding:10px">
+	<h2 style="color:white;text-align:center;">Climate Change Belief Predictor </h2>
+	</div>
+    
+    """
+    
+    st.markdown(html_temp,unsafe_allow_html=True)
+    st.subheader("Predict if a person believes in climate change based on thier tweet")
+    
+    # Creating sidebar with selection box -
+    # you can create multiple pages this way
+    options = ["Prediction", "Information"]
+    selection = st.sidebar.selectbox("Choose Option", options)
 
-	# Creating sidebar with selection box -
-	# you can create multiple pages this way
-	options = ["Prediction", "Information"]
-	selection = st.sidebar.selectbox("Choose Option", options)
+    # Building out the "Information" page
+    if selection == "Information":
+        st.info("General Information")
+        # You can read a markdown file from supporting resources folder
+        st.markdown("Some information here")
 
-	# Building out the "Information" page
-	if selection == "Information":
-		st.info("General Information")
-		# You can read a markdown file from supporting resources folder
-		st.markdown("Some information here")
+        st.subheader("Raw Twitter data and label")
+        if st.checkbox('Show raw data'): # data is hidden if box is unchecked
+            st.write(raw[['sentiment', 'message']]) # will write the df to the page
 
-		st.subheader("Raw Twitter data and label")
-		if st.checkbox('Show raw data'): # data is hidden if box is unchecked
-			st.write(raw[['sentiment', 'message']]) # will write the df to the page
+    # Building out the predication page
+    if selection == "Prediction":
+        #st.info("Prediction with ML Models")
+        # Creating a text box for user input
+        tweet_text = st.text_area("Enter Tweet")
 
-	# Building out the predication page
-	if selection == "Prediction":
-		st.info("Prediction with ML Models")
-		# Creating a text box for user input
-		tweet_text = st.text_area("Enter Text","Type Here")
+        if st.button("Classify"):
+                  
+            # Transforming user input with vectorizer
+            vect_text = [tweet_text]
+            prediction = model.predict(vect_text)
+              
+            if prediction[0] == 0:
+                prediction = 'Neutral'
+                c_img = 'neutral.png'
 
-		if st.button("Classify"):
-			# Transforming user input with vectorizer
-			vect_text = tweet_cv.transform([tweet_text]).toarray()
-			# Load your .pkl file with the model of your choice + make predictions
-			# Try loading in multiple models to give the user a choice
-			predictor = joblib.load(open(os.path.join("resources/Logistic_regression.pkl"),"rb"))
-			prediction = predictor.predict(vect_text)
+            elif prediction[0] == 1:
+                prediction = 'PRO Climate Change'
+                c_img = 'pro.png'
 
-			# When model has successfully run, will print prediction
-			# You can use a dictionary or similar structure to make this output
-			# more human interpretable.
-			st.success("Text Categorized as: {}".format(prediction))
+            elif prediction[0] == 2:
+                prediction = 'Factual News'
+                c_img = 'news.png'
+
+            else:
+                prediction = 'ANTI Climate Change'
+                c_img = 'anti.png'
+  
+
+            # When model has successfully run, will print prediction
+            # You can use a dictionary or similar structure to make this output
+            # more human interpretable.
+            st.success("This Tweet is : {}".format(prediction))
+            load_images(c_img)
+            
 
 # Required to let Streamlit instantiate our web app.  
 if __name__ == '__main__':
-	main()
+    main()
